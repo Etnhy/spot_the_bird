371bd75931fedb7656b8d68f8bde3c3fd727a510
Etnhy <magellan7777@icloud.com>
2022-11-25 17:58:09 +0200

    latlng package

--- lib/screens/map_screens.dart
+++ lib/screens/map_screens.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapScreen extends StatelessWidget {
-  const MapScreen({super.key});
+  // const MapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(0, 0),
+          zoom: 15.3,
+          maxZoom: 17,
+          minZoom: 3.5,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+            retinaMode: true,
+          ),
+        ],
+      ),
     );
   }
 }
