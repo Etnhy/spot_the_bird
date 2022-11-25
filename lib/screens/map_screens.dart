4c9d1f5e60e4fb86a9bf3151b32d7a4b927952c7
Etnhy <magellan7777@icloud.com>
2022-11-25 19:37:56 +0200

    LatLng fix

--- lib/screens/map_screens.dart
+++ lib/screens/map_screens.dart
@@ -3,8 +3,6 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapScreen extends StatelessWidget {
-  // const MapScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
