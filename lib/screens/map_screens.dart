56ebcf3fa0851cb35bcc4a670a5cb34e8373ae93
Etnhy <magellan7777@icloud.com>
2022-11-26 23:49:53 +0200

    -dart js

--- lib/screens/map_screens.dart
+++ lib/screens/map_screens.dart
@@ -1,6 +1,5 @@
 // import 'dart:html';
 import 'dart:io';
-import 'dart:js';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
