bd7535615176a869d289b61036f856cac05a800f
Etnhy <magellan7777@icloud.com>
2022-11-29 19:06:15 +0200

    multi bloc provider

--- lib/bloc/bird_post_cubit.dart
+++ lib/bloc/bird_post_cubit.dart
@@ -7,4 +7,11 @@ part 'bird_post_state.dart';
 class BirdPostCubit extends Cubit<BirdPostState> {
   BirdPostCubit()
       : super(BirdPostState(birdPosts: [], status: BirdPostStatus.initial));
+
+  void addBirdPost(BirdModel birdModel) {
+    emit(state.copyWith(status: BirdPostStatus.loading));
+    List<BirdModel> birdPost = state.birdPosts;
+    birdPost.add(birdModel);
+    emit(state.copyWith(birdPosts: birdPost, status: BirdPostStatus.loaded));
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:spot_the_bird/bloc/bird_post_cubit.dart';
 import 'package:spot_the_bird/bloc/location_cubit.dart';
 import 'package:spot_the_bird/screens/map_screens.dart';
 
@@ -13,8 +14,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<LocationCubit>(
-      create: (BuildContext context) => LocationCubit()..getLocation(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<LocationCubit>(
+          create: (BuildContext context) => LocationCubit()..getLocation(),
+        ),
+        BlocProvider<BirdPostCubit>(
+          create: (BuildContext context) => BirdPostCubit(),
+        )
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
