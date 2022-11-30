68c86f88cf99b74c30131d14a541fd251e27fdbb
Etnhy <magellan7777@icloud.com>
2022-11-30 16:40:12 +0200

    lost

--- lib/bloc/bird_post_cubit.dart
+++ lib/bloc/bird_post_cubit.dart
@@ -2,16 +2,20 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:spot_the_bird/services/sqflite.dart';
 import '../models/bird_model.dart';
 part 'bird_post_state.dart';
 
 class BirdPostCubit extends Cubit<BirdPostState> {
+  final dbHelper = DatabaseHelper.instance;
+
   BirdPostCubit()
       : super(BirdPostState(birdPosts: [], status: BirdPostStatus.initial));
 
   Future<void> loadPosts() async {
     emit(state.copyWith(status: BirdPostStatus.loading));
     List<BirdModel> birdPosts = [];
+
     emit(state.copyWith(birdPosts: birdPosts, status: BirdPostStatus.loaded));
   }
 
